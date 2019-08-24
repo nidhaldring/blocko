c92827b089247a97a6af84d6bc38ab704bb3e5b2
nidhaldring <dringthedreamer@gmail.com>
2019-08-24 16:24:38 +0100

    created timer class

--- timer.js
+++ timer.js
@@ -1,76 +1,46 @@
 
-// we don't aim  to be too precise 
 
-const minute = 60 * 1000; // (ms)
+class Timer{
 
-let timerId;
-let time;
+    constructor(limit){
+        this.limit = limit;
+        this.minute = 60 * 1000; // (ms)
+        this._id = undefined;
+    }
 
-const incrementTimer = () => chrome.browserAction.setBadgeText({text:(time++).toString()});
+    _setBadgeSettings(text,color){
+        chrome.browserAction.setBadgeText({text:text});
+        chrome.browserAction.setBadgeBackgroundColor({color:color});
+    }
 
-function startTimer(){
-    clearInterval(timerId)
-    time = 0;
-    incrementTimer();
-    timerId = setInterval(incrementTimer,minute);
+    _clear(){
+        clearInterval(this._id);
+        this._id = undefined;
+    }
 
-    document.getElementById("start").disabled = true;
-    document.getElementById("pause").disabled = false;
-    document.getElementById("resume").disabled = true;
-    document.getElementById("stop").disabled = false;
+    _increment(){
+        chrome.browserAction.setBadgeText({text:(++this._time) + "m"});
+    }
+
+    start(){
+        this._time = 0;
+        this._setBadgeSettings(this._time + "m","green");
+        this._id = setInterval(this._increment.bind(this),this.minute) ;
+    }
+
+    stop(){
+        this._time = 0;
+        this._clear();
+        this._setBadgeSettings("X","red");
+    }
+
+    pause(){
+        this._setBadgeSettings("P","yellow");
+        this._clear();
+    }
+
+    resume(){
+        this._setBadgeSettings(this._time + "m","green");
+        this._id = setInterval(this._increment,this.minute);
+    }
 }
-
-function stopTimer(){
-    clearInterval(timerId);
-    timerId = undefined;
-    time = 0;
-
-    chrome.browserAction.setBadgeText({text:"--"});
-
-    document.getElementById("start").disabled = false;
-    document.getElementById("pause").disabled = true;
-    document.getElementById("resume").disabled = true;
-    document.getElementById("stop").disabled = true;
-}
-
-function pauseTimer(){
-    clearInterval(timerId);
-    timerId = undefined;
-
-    chrome.browserAction.setBadgeText({text:"P"});
-
-    document.getElementById("start").disabled = true;
-    document.getElementById("pause").disabled = false;
-    document.getElementById("resume").disabled = false;
-    document.getElementById("stop").disabled = false;
-}
-
-function resumeTimer(){
-    timerId = setInterval(() => chrome.browserAction.setBadgeText({text:(++time).toString()}),minute);
-
-    document.getElementById("start").disabled = true;
-    document.getElementById("pause").disabled = false;
-    document.getElementById("resume").disabled = false;
-    document.getElementById("stop").disabled = false;
-}
-
-// create buttons and shit
-function createButton(color,name,onclickFunc){
-    btn = document.createElement("button");
-    btn.innerText = name;
-    btn.id = name;
-    btn.style.backgroundColor = color;
-    btn.style.width = "100%";
-    btn.onclick = onclickFunc;
-    btn.style.display = "block";
-
-    return btn;
-}
-
-document.body.append(
-    createButton("green","start",startTimer),
-    createButton("yellow","pause",pauseTimer),
-    createButton("red","stop",stopTimer),
-    createButton("blue","resume",resumeTimer),
-    createButton("grey","settings",null)
-);
