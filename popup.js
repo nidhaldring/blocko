81abdf26647fdc3bcf663e4fabf788b7fa8de060
nidhaldring <dringthedreamer@gmail.com>
2019-08-25 01:07:53 +0100

    created popup scripts

---
+++ popup.js
@@ -0,0 +1,47 @@
+
+
+function sendMessage(msg){
+    chrome.runtime.sendMessage({text:msg});
+}
+
+function createButton(id,value,color,onclick){
+    let btn = document.createElement("button");
+    btn.id = id;
+    btn.innerText = value;
+    btn.onclick = onclick;
+    btn.style.color = color;
+    btn.style.width = "100%";
+
+    return btn;
+}
+
+function createOnclickFunc(pauseState,resumeState,msg){
+    return () => {
+        const pauseBtn = document.getElementById("pause");
+        const resumeBtn = document.getElementById("resume");
+        
+        pauseBtn.disabled = pauseState;
+        resumeBtn.disabled = resumeState;
+        sendMessage(msg);
+    }
+}
+
+// create buttons
+
+const startBtnOnclick = createOnclickFunc(false,true,"start");
+const startBtn = createButton("start","start","green",startBtnOnclick);
+
+const stopBtnOnclick = createOnclickFunc(true,true,"stop");
+const stopBtn = createButton("stop","stop","red",startBtnOnclick);
+
+const pauseBtnOnclick = createOnclickFunc(true,false,"pause");
+const pauseBtn = createButton("pause","pause","yellow",pauseBtnOnclick);
+
+const resumeBtnOnclick = createOnclickFunc(false,true,"resume");
+const resumeBtn = createButton("resume","resume","blue",resumeBtnOnclick);
+
+
+// main shit
+document.addEventListener("DOMContentLoaded",() => {
+    document.body.append(startBtn,stopBtn,pauseBtn,resumeBtn);
+});
