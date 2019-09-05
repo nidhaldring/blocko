657265d71b8ce01d80c0439a6f08e43be182b183
nidhaldring <dringthedreamer@gmail.com>
2019-09-05 14:59:57 +0100

    refactoring

--- js/config.js
+++ js/config.js
@@ -1,32 +1,51 @@
 
+function validateTextAreaInput(textArea){
+
+    const values = textArea.value.split("\n");
+    const domainReg = /^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$/;
+    const test = values.every((e) => e.match(domainReg));
+
+    if(!test){
+        textArea.focus();
+        alert("invalid input");
+    }   
+}
+
 function initTextArea(){
     chrome.storage.local.get("sites",(res) => {
-        // if first time then leave the placeholder :)
+        const textArea =  document.getElementById("text");
+
         if(res.sites.length > 0){
-            document.getElementById("text").value = res.sites.join("\n");
+           textArea.value = res.sites.join("\n");
         }
+
+        textArea.onchange = () => validateTextAreaInput(textArea);
+    });
+
+}
+
+function initWorkTimeFiled(){
+    chrome.storage.local.get("workTime",(res) => {
+        document.getElementById("workTime").value = res[id]
     });
 }
 
-function initInputFiled(id){
-    chrome.storage.local.get(id,(res) => {
-        document.getElementById(id).value = res[id]
-    });
-}
-
-document.addEventListener("DOMContentLoaded",() => {
-    initTextArea();
-
+function initSaveButton(){
     document.getElementById("save").onclick = () => {
-        let sites = document.getElementById("text").value
-        let workTime = document.getElementById("workTime").value;
+        const sites = document.getElementById("text").value
+        const workTime = document.getElementById("workTime").value;
 
         chrome.storage.local.set({"sites":sites.split("\n")});
         chrome.storage.local.set({"workTime":parseInt(workTime)});
 
         alert("Saved sucessfully !");
     }
+}
 
-    initInputFiled("workTime");
+// main
+document.addEventListener("DOMContentLoaded",() => {
+    initTextArea();
+    initSaveButton();
+    initWorkTimeFiled();
 
 });
