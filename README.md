5d5d894a5e4eb9cd30a00c117175cd1ea36443e6
nidhaldring <dringthedreamer@gmail.com>
2019-09-07 15:55:59 +0100

    added support for motivitional quotes

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ The goal of this extension is to provide a helpful enviroment to be productive, 
 <ul>
 <li> a timer to track how productive you are</li>
 <li> a blacklist to block distracting websites while working </li>
+<li> a list of motivitional quotes </li>
 </ul>
---
+++ js/quotes.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const req = new XMLHttpRequest();
+
+req.open("get",chrome.runtime.getURL("js/quotes.json"));
+
+req.onload = () =>{
+    const quotes = JSON.parse(req.responseText);
+    const quoteObject = quotes[Math.floor(Math.random() * quotes.length)];
+
+    document.getElementById("quote").innerText = quoteObject.quote;
+    document.getElementById("author").innerText =  "- " + quoteObject.name;
+}
+
+req.send();
+
