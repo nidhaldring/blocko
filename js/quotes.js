713f9060cf8769f7fa15d4053a1cba43cd8fadb2
nidhaldring <dringthedreamer@gmail.com>
2019-09-08 20:10:23 +0100

    made quote come with random background

--- js/quotes.js
+++ js/quotes.js
@@ -1,19 +1,29 @@
 
+function createQuote(){
+    const req = new XMLHttpRequest();
 
+    req.open("get",chrome.runtime.getURL("js/quotes.json"));
 
+    req.onload = () =>{
+        const quotes = JSON.parse(req.responseText);
+        const quoteObject = quotes[Math.floor(Math.random() * quotes.length)];
 
+        document.getElementById("quote").innerText = quoteObject.quote;
+        document.getElementById("author").innerHTML = "<q>" + quoteObject.name + "</q>";
+    }
 
-const req = new XMLHttpRequest();
-
-req.open("get",chrome.runtime.getURL("js/quotes.json"));
-
-req.onload = () =>{
-    const quotes = JSON.parse(req.responseText);
-    const quoteObject = quotes[Math.floor(Math.random() * quotes.length)];
-
-    document.getElementById("quote").innerText = quoteObject.quote;
-    document.getElementById("author").innerHTML = "<q>" + quoteObject.name + "</q>";
+    req.send();
 }
 
-req.send();
+function createBackground(){
+    const backgroundsNumber = 1;
+    const backgroundName = "background" + Math.ceil(Math.random() * backgroundsNumber) + ".jpg";
+
+    document.body.style.backgroundImage = `url('../backgrounds/${backgroundName}')`;
+}
+
+// main 
+createBackground();
+createQuote();
+
 
